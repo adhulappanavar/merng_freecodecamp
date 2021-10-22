8a945bf9a8abeab62d8d76959251db469354f8de
Anil Dhulappanavar <dhulappanavar@yahoo.com>
2021-10-22 20:30:12 +0530

    Working : CreateUser and JWT Token generation

---
+++ grahqlserver/resolvers/users.js
@@ -0,0 +1,57 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/users');
+const { SECRET_KEY } = require('../config');
+
+
+
+function generateToken(user) {
+    return jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        username: user.username
+      },
+      SECRET_KEY,
+      { expiresIn: '1h' }
+    );
+  }
+
+
+
+module.exports = {
+    Mutation: {
+
+      async register(
+        _,
+        {
+          registerInput: { username, email, password, confirmPassword }
+        }
+      ) {
+        // Validate user data
+
+        // TODO: Make sure user doesnt already exist
+
+        // hash password and create an auth token
+        password = await bcrypt.hash(password, 12);
+  
+        const newUser = new User({
+          email,
+          username,
+          password,
+          createdAt: new Date().toISOString()
+        });
+  
+        const res = await newUser.save();
+  
+        const token = generateToken(res);
+  
+        return {
+          ...res._doc,
+          id: res._id,
+          token
+        };
+      }
+    }
+  };
