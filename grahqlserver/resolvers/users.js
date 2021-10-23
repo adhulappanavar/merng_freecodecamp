68c2922ebb60bce1eaaf8fc78c59f7f669afceb6
Anil Dhulappanavar <dhulappanavar@yahoo.com>
2021-10-23 10:53:59 +0530

    Final Code for Register and Login

--- grahqlserver/resolvers/users.js
+++ grahqlserver/resolvers/users.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
 
 const User = require('../models/users');
-const {validateRegisterInput} = require ('../utils/validators')
+const {validateRegisterInput, validateLoginInput} = require ('../utils/validators')
 
 const { SECRET_KEY } = require('../config');
 
@@ -25,6 +25,35 @@ function generateToken(user) {
 
 module.exports = {
     Mutation: {
+
+        async login(_, { username, password }) {
+            const { errors, valid } = validateLoginInput(username, password);
+      
+            if (!valid) {
+              throw new UserInputError('Errors', { errors });
+            }
+      
+            const user = await User.findOne({ username });
+      
+            if (!user) {
+              errors.general = 'User not found';
+              throw new UserInputError('User not found', { errors });
+            }
+      
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) {
+              errors.general = 'Wrong crendetials';
+              throw new UserInputError('Wrong crendetials', { errors });
+            }
+      
+            const token = generateToken(user);
+      
+            return {
+              ...user._doc,
+              id: user._id,
+              token
+            };
+          },
 
       async register(
         _,
