158f0c12a9bb5d529bf9fb8a62718c1de256ff58
Anil Dhulappanavar <dhulappanavar@yahoo.com>
2021-10-24 12:02:53 +0530

    4a Comments on Posts added

--- grahqlserver/typeDefs.js
+++ grahqlserver/typeDefs.js
@@ -6,7 +6,23 @@ module.exports = gql`
         body: String!,
         username: String!,
         createdAt: String!
+        comments: [Comment]!
+        likes: [Like]!
     }
+
+    type Comment {
+        id: ID!
+        createdAt: String!
+        username: String!
+        body: String!
+    }
+    type Like {
+        id: ID!
+        createdAt: String!
+        username: String!
+    }
+
+
     type Query{
         getPosts: [Post]
         getPost(postId: ID!): Post
@@ -33,5 +49,8 @@ module.exports = gql`
         login(username: String!, password: String!): User!
         createPost(body: String!): Post!
         deletePost(postId: ID!): String!
+        createComment(postId: String!, body: String!): Post!
+        deleteComment(postId: ID!, commentId: ID!): Post!
+        likePost(postId: ID!): Post!
     }
 `;
