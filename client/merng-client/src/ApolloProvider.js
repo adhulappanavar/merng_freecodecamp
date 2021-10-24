b20bf010f979c0962ff548702f9df6b501a32d4e
Anil Dhulappanavar <dhulappanavar@yahoo.com>
2021-10-24 17:22:25 +0530

    5c-Updated Graohql server URL

---
+++ client/merng-client/src/ApolloProvider.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import App from './App';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    // useQuery,
+    // gql
+  } from "@apollo/client";
+
+// const client = ApolloClient({
+//   uri: 'http://localhost:5000'
+// });
+
+
+
+const client = new ApolloClient({
+    uri: "http://localhost:5000",
+    cache: new InMemoryCache()
+  });
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
